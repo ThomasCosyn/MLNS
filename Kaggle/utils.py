3ba3381a08c5992449e05d97f8701a505321e462
Thomas Cosyn <thomas.cosyn@student-cs.fr>
2023-03-25 12:32:22 +0100

    Ajout de pleins de features, belle amélioration des perfs

--- Kaggle/utils.py
+++ Kaggle/utils.py
@@ -1,5 +1,19 @@
 import networkx as nx
+import numpy as np
 import pandas as pd
+from scipy.sparse import identity
+from scipy.sparse.linalg import inv
+
+def adamic_adar(graph, node1, node2):
+
+    c = 0
+    gamma_x = set(graph.neighbors(node1))
+    gamma_y = set(graph.neighbors(node2))
+    for z in gamma_x & gamma_y:
+        if len(set(graph.neighbors(z))) > 1:
+            c += 1 / np.log(len(set(graph.neighbors(z))))
+
+    return c
 
 def compute_max_flow(row, graph):
     
@@ -8,6 +22,16 @@ def compute_max_flow(row, graph):
 
     flow_value, flow_dict = nx.maximum_flow(graph, _s = row['node1'], _t = row['node2'])
     return flow_value
+
+def katzB_matrix(graph, beta):
+
+    A = nx.adjacency_matrix(graph, nodelist = graph.nodes)
+    nodes_index = {list(graph.nodes)[i]: i for i in range(graph.number_of_nodes())}
+    nodes_index_reverse = {i: list(graph.nodes)[i] for i in range(graph.number_of_nodes())}
+    I = identity(A.shape[0], format = 'csr')
+    M = inv(I - beta * A) - I
+
+    return M, nodes_index, nodes_index_reverse
 
 def text_to_graph(filepath):
     """
